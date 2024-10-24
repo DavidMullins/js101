634387d54858452ed9b5e533aa49c0e9ff1cf82e
David Mullins <mullinsdavid88@gmail.com>
2024-10-24 08:41:57 -0400

    Add pseudocode assignment

---
+++ lesson_2/pseudocode.js
@@ -0,0 +1,84 @@
+/* 
+*** A function that returns the sum of two numbers
+***
+
+- Create a function with 2 parameters, num1 and num2.
+
+- Veryify num1 is of type Number
+--  If not, attempt to coerce it to an integer and re-verify.
+--- If num1 is still not an interger, return statement as such to user.
+
+- Veryify num2 is of type Number
+-- If not, attempt to coerce it to an integer and re-verify.
+--- If num2 is still not an interger, return statement as such to user.
+
+- Within the body, return num1 + num2
+
+
+
+*** A function that takes an array of strings, and returns a string
+that is all those strings concatenated together
+***
+
+- Create a fucntion that takes an array as a parameter
+
+- Verify the arguemement passed into the function is an array of strings
+
+- create an empty string variable with let
+
+- loop through array and on each iteration add the element to the empty string
+array. make sure to concat
+
+- after looping return the string variable you added to
+
+
+
+*** A method that takes an array of integers, and returns a new array with
+every other element from the original array, starting with the first element.
+***
+
+- Create a fucntion that takes an array as a parameter
+
+- Verify the arguemement passed into the function is an array of numbers
+
+- create an empty array variable
+
+- loop through array starting with index 0 and add the element to the new 
+array variable created. += 2 for iteration. repeat.
+
+- after looping return the array variable you added to
+
+
+
+*** A function that determines the index of the 3rd occurrence of a given
+character in a string.
+***
+
+- create a fucntion that takes 2 parameters, a string, and a character
+
+- intialize a counting variable to 0
+
+- intialize an index variable to 0
+
+- loop though string starting at its index of 0
+-- each loop check IF the string at index of the loop is equal to the character arguement passed
+-- if so, add to the count 1
+-- after adding to count, check if count is 3
+--- if so, set index variable from earlier to the index of current point in loop and break
+out of loop
+- finally, return the index variable
+
+
+
+*** A function that takes two arrays of numbers and returns the result of
+merging the arrays.
+*** 
+
+- create a fucntion that takes 2 arrays as parameters
+
+- verify both arrays are of numbers
+
+- return array1.concat(array2)
+
+
+*/
