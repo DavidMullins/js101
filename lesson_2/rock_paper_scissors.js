17f23ab8a1396e1d89bcf44a89716ef1e31b12c7
David Mullins <mullinsdavid88@gmail.com>
2024-11-22 01:13:24 -0500

    Set up lesson 3 directory

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -4,18 +4,8 @@ const VALID_CHOICES = ['rock', 'paper', 'scissors'];
 function prompt(msg) {
   console.log(`=> ${msg}`);
 }
-while (true) {
-  prompt(`Chose one: ${VALID_CHOICES.join(', ')}`);
-  let choice = readline.question();
 
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt("That's not a valid choice");
-    choice = readline.question();
-  }
-
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
-
+function displayWinner (choice, computerChoice) {
   prompt(`You chose ${choice}, computer chose ${computerChoice}`);
 
   if ((choice === 'rock' && computerChoice === 'scissors') ||
@@ -29,6 +19,21 @@ while (true) {
   } else {
     prompt("It's a tie");
   }
+}
+
+while (true) {
+  prompt(`Chose one: ${VALID_CHOICES.join(', ')}`);
+  let choice = readline.question();
+
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt("That's not a valid choice");
+    choice = readline.question();
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  displayWinner(choice, computerChoice);
 
   prompt('Do you want to play again?');
   let answer = readline.question().toLowerCase();
